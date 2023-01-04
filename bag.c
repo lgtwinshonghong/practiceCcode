a32c1138078ea9d239c45dc15b4bd5c2762658bf
lgtwinshonghong <hynmail@naver.com>
2023-01-04 22:35:14 +0900

    first commit

---
+++ bag.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   
+    int item, value;
+    printf("Enter the total items number :");
+    scanf("%d",&item);
+    printf("Enter the hitght weight :");
+    scnaf("%d",&value);
+    printf("Total item %d and the hight weight is %d ",item,value);
+    return 0;
+}
