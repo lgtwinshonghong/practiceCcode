f127f6b37834504927ad952c2578e12e9ec3969b
lgtwinshonghong <hynmail@naver.com>
2023-01-04 23:59:12 +0900

    check scanf

--- bag.c
+++ bag.c
@@ -3,11 +3,13 @@
 
 int main(){
    
-    int item, value;
+    int item =2 , value=3;
     printf("Enter the total items number :");
-    scanf("%d",&item);
+    //scanf("%d",&item);
+     printf("\n");
     printf("Enter the hitght weight :");
-    scnaf("%d",&value);
+    //scnaf("%d",&value);
+     printf("\n");
     
     printf("Total item %d and the hight weight is %d ",item,value);
     
